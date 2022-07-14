22ed4e6026d415f153c25cc00b720d97f7ca4bdb
Sveta <svet.lukyantseva@gmail.com>
2022-07-15 02:53:31 +0300

    refactor: toggle properties function

--- todo/src/components/TodoList/TodoList.js
+++ todo/src/components/TodoList/TodoList.js
@@ -3,7 +3,8 @@ import React from 'react';
 import TodoListItem from '../TodoListItem';
 import './TodoList.css';
 
-const TodoList = ({ todos, onDeleted }) => {
+const TodoList = ({ todos, onDeleted,
+    onToggleImportant, onToggleDone }) => {
 
     const elements = todos.map((item) => {
         const { id, ...itemProps } = item;
@@ -12,7 +13,9 @@ const TodoList = ({ todos, onDeleted }) => {
             <li key={id} className="list-group-item">
                 <TodoListItem
                     {...itemProps}
-                    onDeleted={() => onDeleted(id)} />
+                    onDeleted={() => onDeleted(id)}
+                    onToggleDone={() => onToggleDone(id)}
+                    onToggleImportant={() => onToggleImportant(id)}/>
             </li>
         );
     });
